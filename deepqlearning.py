6c2ea68e81bf7af6aaeed90cfd6cfc34f91b5396
Orhan CINAR <orhancinar35@gmail.com>
2019-10-10 16:50:26 +0200

    updates

--- deepqlearning.py
+++ deepqlearning.py
@@ -9,11 +9,27 @@ import tensorflow as tf
 
 import random
 
-REPLAY_MEMORY_SIZE = 50_000
-MODEL_NAME = "256x2"
-MINIBATCH_SIZE = 64
 DISCOUNT = 0.99
-UPDATE_TARGET_EVERY = 5
+REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
+# Minimum number of steps in a memory to start training
+MIN_REPLAY_MEMORY_SIZE = 1_000
+MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
+MODEL_NAME = '2x256'
+MIN_REWARD = -200  # For model save
+MEMORY_FRACTION = 0.20
+
+# Environment settings
+EPISODES = 20_000
+
+# Exploration settings
+epsilon = 1  # not a constant, going to be decayed
+EPSILON_DECAY = 0.99975
+MIN_EPSILON = 0.001
+
+#  Stats settings
+AGGREGATE_STATS_EVERY = 50  # episodes
+SHOW_PREVIEW = False
 
 env = BlobEnv()
 
@@ -100,18 +116,15 @@ class ModifiedTensorBoard(TensorBoard):
     def update_stats(self, **stats):
         self._write_logs(stats, self.step)
 
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
 
-def update_replay_memory(self, transition):
-    self.replay_memory.append(transition)
+    def get_qs(self, state, step):
+        return self.model_predict(np.array(state).reshape(-1, *state.shape)/255)[0]
 
-
-def get_qs(self, state, step):
-    return self.model_predict(np.array(state).reshape(-1, *state.shape)/255)[0]
-
-
-def train(self, terminal_state, step):
-    if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-        return
+    def train(self, terminal_state, step):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
 
     minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
@@ -149,6 +162,72 @@ def train(self, terminal_state, step):
     if self.target_update_counter > UPDATE_TARGET_EVERY:
         self.target_model.set_weights(self.model.get_weights())
         self.target_update_counter = 0
+
+
+class Blob:
+    def __init__(self, size):
+        self.size = size
+        self.x = np.random.randint(0, size)
+        self.y = np.random.randint(0, size)
+
+    def __str__(self):
+        return f"Blob ({self.x}, {self.y})"
+
+    def __sub__(self, other):
+        return (self.x-other.x, self.y-other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def action(self, choice):
+        '''
+        Gives us 9 total movement options. (0,1,2,3,4,5,6,7,8)
+        '''
+        if choice == 0:
+            self.move(x=1, y=1)
+        elif choice == 1:
+            self.move(x=-1, y=-1)
+        elif choice == 2:
+            self.move(x=-1, y=1)
+        elif choice == 3:
+            self.move(x=1, y=-1)
+
+        elif choice == 4:
+            self.move(x=1, y=0)
+        elif choice == 5:
+            self.move(x=-1, y=0)
+
+        elif choice == 6:
+            self.move(x=0, y=1)
+        elif choice == 7:
+            self.move(x=0, y=-1)
+
+        elif choice == 8:
+            self.move(x=0, y=0)
+
+    def move(self, x=False, y=False):
+
+        # If no value for x, move randomly
+        if not x:
+            self.x += np.random.randint(-1, 2)
+        else:
+            self.x += x
+
+        # If no value for y, move randomly
+        if not y:
+            self.y += np.random.randint(-1, 2)
+        else:
+            self.y += y
+
+        # If we are out of bounds, fix!
+        if self.x < 0:
+            self.x = 0
+        elif self.x > self.size-1:
+            self.x = self.size-1
+        if self.y < 0:
+            self.y = 0
+        elif self.y > self.size-1:
+            self.y = self.size-1
 
 
 class BlobEnv:
@@ -232,3 +311,55 @@ class BlobEnv:
         # reading to rgb. Apparently. Even tho color definitions are bgr. ???
         img = Image.fromarray(env, 'RGB')
         return img
+
+
+agent = DQNAgent()
+
+for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit="episode"):
+    agent.tensorboard.step = episode
+
+    episode_reward = 0
+    step = 1
+    current_state = env.reset
+
+    done = False
+
+    while not done:
+        if np.random.random() > epsilon:
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            action = np.random.randint(0, env.ACTION_SPACE_SIZE)
+
+        new_state, reward, done = env.step(action)
+
+        episode_reward += reward
+
+        if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+            env.render()
+
+        agent.update_replay_memory(
+            (current_state, actioni, reward, new_state, done))
+        agent.train(done, step)
+
+        current_state = new_state
+        step += 1
+
+      # Append episode reward to a list and log stats (every given number of episodes)
+    ep_rewards.append(episode_reward)
+    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+        average_reward = sum(
+            ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        agent.tensorboard.update_stats(
+            reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+
+        # Save model, but only when min reward is greater or equal a set value
+        if min_reward >= MIN_REWARD:
+            agent.model.save(
+                f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+    # Decay epsilon
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
