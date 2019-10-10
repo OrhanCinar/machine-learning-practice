85315334a1779d2e6a037cd24568e338812b98fb
orhanc <orhancinar35@gmail.com>
2019-10-10 17:23:47 +0200

    update

--- deepqlearning.py
+++ deepqlearning.py
@@ -1,12 +1,15 @@
-from keras.model import Sequential
+from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 from collections import deque
 import time
 import numpy as np
+import os 
+import cv2
+from PIL import Image
+from tqdm import tqdm 
 import tensorflow as tf
-
 import random
 
 DISCOUNT = 0.99
@@ -31,8 +34,6 @@ MIN_EPSILON = 0.001
 AGGREGATE_STATS_EVERY = 50  # episodes
 SHOW_PREVIEW = False
 
-env = BlobEnv()
-
 # For stats
 ep_rewards = [-200]
 
@@ -45,123 +46,8 @@ tf.set_random_seed(1)
 # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
 # backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
 
-# Create models folder
-if not os.path.isdir('models'):
-    os.makedirs('models')
 
 
-class DQNAgent:
-    def __init__(self):
-        self.model = self.create_model()
-
-        self.target_model = self.create_model()
-        self.target_model.set_weights(self.model.get_weights())
-
-        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
-
-        self.tensorboard = ModifiedTensorBoard(
-            log_dir=f"logs/{MODEL_NAME}-{time.time()}")
-
-        self.target_update_counter = 0
-
-    def create_model(self):
-        model = Sequential()
-        model.add(Conv2D(256, (3, 3), input_shape=env.OBSERVATION_SPACE_VALUES))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(2, 2))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(256, (3, 3)))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(2, 2))
-        model.add(Dropout(0.2))
-
-        model.add(Flatten())
-        model.add(Dense(64))
-
-        model.add(Dense(env.ACTION_SPACE_SIZE, activation="linear"))
-        model.compile(loss="mse", optimizer=Adam(
-            lr=0.001), metrics=["accuracy"])
-        return model
-
-
-class ModifiedTensorBoard(TensorBoard):
-
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.step = 1
-        self.writer = tf.summary.FileWriter(self.log_dir)
-
-    # Overriding this method to stop creating default log writer
-    def set_model(self, model):
-        pass
-
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
-    def on_epoch_end(self, epoch, logs=None):
-        self.update_stats(**logs)
-
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
-    def on_batch_end(self, batch, logs=None):
-        pass
-
-    # Overrided, so won't close writer
-    def on_train_end(self, _):
-        pass
-
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
-    def update_stats(self, **stats):
-        self._write_logs(stats, self.step)
-
-    def update_replay_memory(self, transition):
-        self.replay_memory.append(transition)
-
-    def get_qs(self, state, step):
-        return self.model_predict(np.array(state).reshape(-1, *state.shape)/255)[0]
-
-    def train(self, terminal_state, step):
-        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-            return
-
-    minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-
-    current_states = np.array([transition[0]
-                               for transition in minibatch]) / 255
-
-    curent_qs_list = self.model.predict(current_states)
-
-    new_current_states = np.array([transition[3]
-                                   for transition in minibatch]) / 255
-
-    future_qs_list = self.target_model.predict(new_current_states)
-
-    x = []
-    y = []
-
-    for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
-        if not done:
-            max_future_q = np.max(future_qs_list[index])
-            new_q = reward + DISCOUNT * max_future_q
-        else:
-            new_q = reward
-
-        current_qs = current_qs_list[index]
-        current_qs[action] = new_q
-
-        x.append(current_state)
-        y.append(current_qs)
-    self.model.fit(np.array(x)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0,
-                   shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
-
-    if terminal_state:
-        self.target_update_counter += 1
-
-    if self.target_update_counter > UPDATE_TARGET_EVERY:
-        self.target_model.set_weights(self.model.get_weights())
-        self.target_update_counter = 0
 
 
 class Blob:
@@ -311,6 +197,131 @@ class BlobEnv:
         # reading to rgb. Apparently. Even tho color definitions are bgr. ???
         img = Image.fromarray(env, 'RGB')
         return img
+
+
+
+env = BlobEnv()
+
+
+# Create models folder
+if not os.path.isdir('models'):
+    os.makedirs('models')
+
+class DQNAgent:
+    def __init__(self):
+        self.model = self.create_model()
+
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        self.tensorboard = ModifiedTensorBoard(
+            log_dir=f"logs/{MODEL_NAME}-{time.time()}")
+
+        self.target_update_counter = 0
+
+    def create_model(self):
+        model = Sequential()
+        model.add(Conv2D(256, (3, 3), input_shape=env.OBSERVATION_SPACE_VALUES))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(2, 2))
+        model.add(Dropout(0.2))
+
+        model.add(Conv2D(256, (3, 3)))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(2, 2))
+        model.add(Dropout(0.2))
+
+        model.add(Flatten())
+        model.add(Dense(64))
+
+        model.add(Dense(env.ACTION_SPACE_SIZE, activation="linear"))
+        model.compile(loss="mse", optimizer=Adam(
+            lr=0.001), metrics=["accuracy"])
+        return model
+
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    def get_qs(self, state, step):
+        return self.model_predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+
+    def train(self, terminal_state, step):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        current_states = np.array([transition[0]
+                               for transition in minibatch]) / 255
+
+        curent_qs_list = self.model.predict(current_states)
+
+        new_current_states = np.array([transition[3]
+                                   for transition in minibatch]) / 255
+
+        future_qs_list = self.target_model.predict(new_current_states)
+
+        x = []
+        y = []
+
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+                current_qs = current_qs_list[index]
+                current_qs[action] = new_q
+
+                x.append(current_state)
+                y.append(current_qs)
+            self.model.fit(np.array(x)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0,
+                        shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
+
+            if terminal_state:
+                self.target_update_counter += 1
+
+            if self.target_update_counter > UPDATE_TARGET_EVERY:
+                self.target_model.set_weights(self.model.get_weights())
+                self.target_update_counter = 0
+
+    
+
+
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
 
 
 agent = DQNAgent()
