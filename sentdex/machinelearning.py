6babcf7da03ce9407b9674039142df242e3b4b89
Orhan CINAR <orhancinar35@gmail.com>
2020-01-06 02:07:09 +0100

    updates

--- sentdex/machinelearning.py
+++ sentdex/machinelearning.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import quandl as q
 import math
+import datetime
 import numpy as np
 from sklearn import preprocessing,  svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
 
 df = q.get('WIKI/GOOGL')
 
@@ -23,17 +28,47 @@ forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-df.dropna(inplace=True)
-
-X = np.array(df.drop('label', 1))
-y = np.array(df['label'])
+X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 clf = LinearRegression(n_jobs=10)
 clf.fit(X_train, y_train)
+
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf)
+
+pickle_in = open('linearregression.pickle', 'rb')
+
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
-# print(df.head())
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
