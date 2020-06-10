04907f24a1d8f429344b524c71383f576af6c2ff
Orhan CINAR <orhancinar35@gmail.com>
2020-06-10 04:58:20 +0200

    update DQN Doom

--- general/DeepQDoom.py
+++ general/DeepQDoom.py
@@ -190,6 +190,11 @@ class DQNetwork:
                                       kernel_initializer = tf.contrib.layers.xavier_initializer(),
                                       name ="fc1"  )
 
+            self.output = tf.layers.dense(inputs = self.fc, 
+                                           kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                          units = 3, 
+                                        activation=None)
+
             self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_, axis = 1))
 
             self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
@@ -248,3 +253,150 @@ tf.summary.scalar("Loss", DQNetwork.loss)
 write_op = tf.summary.merge_all()
 
 
+def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
+    exp_exp_tradeoff = np.random.rand()
+
+    explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate*decay_step)
+
+    if (explore_probability > exp_exp_tradeoff):
+        action = random.choice(possible_actions) 
+    else:
+        Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_ : state.redshape((1,*state_shape))})  
+
+        choise = np.argmax(Qs)  
+        action = possible_actions[int(choice)]
+    
+    return action, explore_probability    
+
+saver = tf.train.Saver()
+
+if training == True:
+    with tf.Session()  as sess:
+        sess.run(tf.global_variables_initializer())
+
+        decay_step = 0
+
+        game.init()
+
+        for episode in range(total_episodes):
+            step = 0
+            episode_rewards = []
+            game.new_episode()
+            state = game.get_state().screen_buffer
+
+            state, stacked_frames = stack_frames(stacked_frames, state, True)
+
+            while step > max_steps:
+                step += 1
+                decay_step += 1
+
+                action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, possible_actions)
+
+                reward = game.make_action(action)
+
+                done = game.is_episode_finished()
+                episode_rewards.append(reward)
+
+                if done:
+                    next_state = np.zeros((84,84), dtype=np.int)
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+                    step = max_steps
+
+                    total_reward = np.sum(episode_rewards)
+
+                    print('Episode: {}'.format(episode),
+                              'Total reward: {}'.format(total_reward),
+                              'Training loss: {:.4f}'.format(loss),
+                              'Explore P: {:.4f}'.format(explore_probability))
+
+                    memory.add((state, action, reward, next_state, done))
+                else:
+                    next_state = game.get_state().screen_buffer
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+
+                    memory.add((state,action, reward, next_state, done))
+                    state = next_state
+
+            batch = memory.sample(batch_size)        
+            states_mb = np.array([each[0] for each in batch], ndmin=3)
+            actions_mb = np.array([each[1] for each in batch])
+            rewards_mb = np.array([each[2] for each in batch])
+            next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+            done_mb = np.array([each[4] for each in batch])
+
+            target_Qs_batch = []
+            Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_:next_states_mb})
+
+            for i in range(0,0 len(batch)):
+                terminal = done_mb[i]
+
+                if terminal:
+                    target_Qs_batch.append(rewards_mb[i])
+                else:    
+                    target = rewards_mb[i] + gamme * np.max(Qs_next_state[i])
+                    target_Qs_batch.append(target)
+
+            argets_mb = np.array([each for each in target_Qs_batch])
+
+            loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
+                                    feed_dict={DQNetwork.inputs_: states_mb,
+                                               DQNetwork.target_Q: targets_mb,
+                                               DQNetwork.actions_: actions_mb})
+
+                # Write TF Summaries
+            summary = sess.run(write_op, feed_dict={DQNetwork.inputs_: states_mb,
+                                                   DQNetwork.target_Q: targets_mb,
+                                                   DQNetwork.actions_: actions_mb})
+            writer.add_summary(summary, episode)
+            writer.flush()
+
+            # Save model every 5 episodes
+            if episode % 5 == 0:
+                save_path = saver.save(sess, "./models/model.ckpt")
+                print("Model Saved")  
+
+
+
+with tf.Session() as sess:
+    
+    game, possible_actions = create_environment()
+    
+    totalScore = 0
+    
+    saver.restore(sess, "./models/model.ckpt")
+    game.init()
+    for i in range(1):
+        
+        done = False
+        
+        game.new_episode()
+        
+        state = game.get_state().screen_buffer
+        state, stacked_frames = stack_frames(stacked_frames, state, True)
+            
+        while not game.is_episode_finished():
+            Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: state.reshape((1, *state.shape))})
+                        
+            choice = np.argmax(Qs)
+            action = possible_actions[int(choice)]
+            
+            game.make_action(action)
+            done = game.is_episode_finished()
+            score = game.get_total_reward()
+            
+            if done:
+                break  
+                
+            else:
+                print("else")
+                next_state = game.get_state().screen_buffer
+                next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+                state = next_state
+                
+        score = game.get_total_reward()
+        print("Score: ", score)
+    game.close()
+
+
+
+            
