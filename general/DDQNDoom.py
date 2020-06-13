f9b124185ec8feca08aa168545f068cdbaf2f999
Orhan CINAR <orhancinar35@gmail.com>
2020-06-13 04:31:09 +0200

    update ddqn doom

--- general/DDQNDoom.py
+++ general/DDQNDoom.py
@@ -97,3 +97,109 @@ memory_size = 100000
 training = False
 episode_render = False
 
+
+class DDDQNet:
+    def __init__(self, state_size, action_size, learning_rate, name):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.learning_rate = learning_rate,
+        self.name = name
+
+        with tf.variable_scope(self.name):
+            self.inputs_ = tf.placeholder(
+                tf.float32, [None, *state_size], name="inputs")
+            self.ISWeigths_ = tf.placeholder(
+                tf.float32, [None, 1], name="IS_Weigths")
+            self.actions_ = tf.placeholder(
+                tf.float32, [None, state_size], name="actions_")
+            self.target_Q = tf.placeholder(tf.float32, [None], name="target")
+
+            """
+            First convnet:
+            CNN
+            ELU
+            """
+
+            self.conv1 = tf.layers.conv2d(inputs=self.inputs_,
+                                          filters=32,
+                                          kernel_size=[4, 4],
+                                          padding="VALID",
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                          name="conv1")
+            self.conv1_out = tf.nn.elu(self.conv1, name="conv1_out")
+
+            """
+            Second convnet:
+            CNN
+            ELU
+            """
+            self.conv2 = tf.layers.conv2d(inputs=self.conv1_out,
+                                          filters=64,
+                                          kernel_size=[4, 4],
+                                          strides=[2, 2],
+                                          padding="VALID",
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                          name="conv2")
+            self.conv2_out = tf.nn.elu(self.conv2, name="conv2_out")
+
+            """
+            Third convnet:
+            CNN
+            ELU
+            """
+            self.conv3 = tf.layers.conv2d(inputs=self.conv2_out,
+                                          filters=128,
+                                          kernel_size=[4, 4],
+                                          strides=[2, 2],
+                                          padding="VALID",
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                          name="conv3")
+            self.conv3_out = tf.nn.elu(self.conv2, name="conv3_out")
+
+            self.flatten = tf.layers.flatten(self.conv3_out)
+
+            self.value_fc = tf.layers.dense(inputs=self.flatten,
+                                            units=512,
+                                            activation=None,
+                                            kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                            name="value_fc")
+
+            self.value = tf.layers.dense(inputs=self.value_fc,
+                                         units=512,
+                                         activation=tf.nn.elu,
+                                         kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                         name="value")
+
+            self.advantage_fc = tf.layers.dense(inputs=self.flatten,
+                                                units=512,
+                                                activation=tf.nn.elu,
+                                                kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                                name="advantage_fc")
+
+            self.advantage = tf.layers.dense(inputs=self.advantage_fc,
+                                             units=self.action_size,
+                                             activation=None,
+                                             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                             name="advantages")
+
+            self.output = self.value + \
+                tf.subtract(self.advantage, tf.reduce_mean(
+                    self.advantage, axis=1, keepdims=True))
+
+            self.Q = tf.reduce_sum(tf.multiply(
+                self.output, self.actions_), axis=1)
+
+            self.absolute_errors = tf.abs(self.target_Q - self.Q)
+
+            self.loff = tf.reduce_mean(
+                self.ISWeights_ * tf.squared_difference(self.target_Q), self.Q)
+
+            self.optimizer = tf.train.RMSPropOptimizer(
+                self.learning_rate).minimize(self.loss)
+
+
+tf.reset_default_graph()
+DQNetwork = DDDQNet(state_size, action_size, learning_rate, name="DQNetwork")
+
+TargetNetwork = DDDQNet(state_size, action_size,
+                        learning_rate, name="TargetNetwork")
