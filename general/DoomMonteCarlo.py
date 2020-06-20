874acaee3504a595a1d88667c11579465d8d1e05
Orhan CINAR <orhancinar35@gmail.com>
2020-06-20 02:19:21 +0200

    update doom monte carlo

--- general/DoomMonteCarlo.py
+++ general/DoomMonteCarlo.py
@@ -99,7 +99,7 @@ gamme = 0.95
 training = True
 
 
-def PQNetwork:
+class PQNetwork:
     def __init__(self, state_size, action_size, learning_rate, name="PQNetwork"):
         self.state_size = state_size
         self.action_size = action_size
@@ -123,83 +123,90 @@ def PQNetwork:
                BatchNormalization
                ELU
                """
-               self.conv1 = tf.layers.conv2d(inputs = self.inputs_,
-                                             filters = 32,
-                                             kernel_size = [8,8],
-                                             strides = [4,4],
-                                             padding="VALID", 
-                                             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                             name = "conv1")
-               self.conv1_batchnorm = tf.layers.batch_normalization(self.conv1,
-                                                                     training = True,
-                                                                     epsilon = 1e-5,
-                                                                     name = "batch_norm1")
-               self.conv1_out = tf.nn.elu(self.conv1_batchnorm, name = "conv1_out")     ## --> [20, 20, 32]                                                                 
+                self.conv1 = tf.layers.conv2d(inputs=self.inputs_,
+                                              filters=32,
+                                              kernel_size=[8, 8],
+                                              strides=[4, 4],
+                                              padding="VALID",
+                                              kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                              name="conv1")
+                self.conv1_batchnorm = tf.layers.batch_normalization(self.conv1,
+                                                                     training=True,
+                                                                     epsilon=1e-5,
+                                                                     name="batch_norm1")
+                self.conv1_out = tf.nn.elu(
+                    self.conv1_batchnorm, name="conv1_out")  # --> [20, 20, 32]
 
             with tf.name_scope("conv2"):
-              """
-                Second convnet:
-                CNN
-                BatchNormalization
-                ELU
                 """
-               self.conv2 = tf.layers.conv2d(inputs = self.conv1_out,
-                                             filters = 64,
-                                             kernel_size = [4,4],
-                                             strides = [2,2],
-                                             padding="VALID", 
-                                             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                             name = "conv2")
-               self.conv2_batchnorm = tf.layers.batch_normalization(self.conv2,
-                                                                     training = True,
-                                                                     epsilon = 1e-5,
-                                                                     name = "batch_norm2")
-               self.conv2_out = tf.nn.elu(self.conv1_batchnorm, name = "conv2_out")     ## --> [9, 9, 64]
+                  Second convnet:
+                  CNN
+                  BatchNormalization
+                  ELU
+                  """
+                self.conv2 = tf.layers.conv2d(inputs=self.conv1_out,
+                                              filters=64,
+                                              kernel_size=[4, 4],
+                                              strides=[2, 2],
+                                              padding="VALID",
+                                              kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                              name="conv2")
+                self.conv2_batchnorm = tf.layers.batch_normalization(self.conv2,
+                                                                     training=True,
+                                                                     epsilon=1e-5,
+                                                                     name="batch_norm2")
+                self.conv2_out = tf.nn.elu(
+                    self.conv1_batchnorm, name="conv2_out")  # --> [9, 9, 64]
 
             with tf.name_scope("conv3"):
-              """
-                Third convnet:
-                CNN
-                BatchNormalization
-                ELU
                 """
-               self.conv3 = tf.layers.conv2d(inputs = self.conv3_out,
-                                             filters = 128,
-                                             kernel_size = [4,4],
-                                             strides = [2,2],
-                                             padding="VALID", 
-                                             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                             name = "conv3")
-               self.conv3_batchnorm = tf.layers.batch_normalization(self.conv2,
-                                                                     training = True,
-                                                                     epsilon = 1e-5,
-                                                                     name = "batch_norm3")
-               self.conv3_out = tf.nn.elu(self.conv1_batchnorm, name = "conv3_out")     ## --> [3, 3, 128]
+                  Third convnet:
+                  CNN
+                  BatchNormalization
+                  ELU
+                  """
+                self.conv3 = tf.layers.conv2d(inputs=self.conv3_out,
+                                              filters=128,
+                                              kernel_size=[4, 4],
+                                              strides=[2, 2],
+                                              padding="VALID",
+                                              kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                              name="conv3")
+                self.conv3_batchnorm = tf.layers.batch_normalization(self.conv2,
+                                                                     training=True,
+                                                                     epsilon=1e-5,
+                                                                     name="batch_norm3")
+                self.conv3_out = tf.nn.elu(
+                    self.conv1_batchnorm, name="conv3_out")  # --> [3, 3, 128]
 
             with tf.name_scope("flatten"):
-                self.flatten = tf.layers.flatten(self.conv3_out)  ## --> [1152]
+                self.flatten = tf.layers.flatten(self.conv3_out)  # --> [1152]
 
             with tf.name_scope("fc1"):
-                self.fc = tf.layers.dense(inputs= self.flatten,
-                                             units = 512,
-                                             activation = tf.nn.elu,
-                                             kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                             name="fc1")
+                self.fc = tf.layers.dense(inputs=self.flatten,
+                                          units=512,
+                                          activation=tf.nn.elu,
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                          name="fc1")
 
             with tf.name_scope("logits"):
-                self.logits = tf.layers.dense(inputs= self.fc,                                                                                          
-                                             kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                             units = 3,
-                                             activation = None)
+                self.logits = tf.layers.dense(inputs=self.fc,
+                                              kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                              units=3,
+                                              activation=None)
             with tf.name_scope("softmax"):
                 self.action_distribution = tf.nn.softmax(self.logits)
 
             with tf.name_scope("loss"):
-                self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits(logits = self.logits, labels = self.actions)
-                self.loss = tf.reduce_mean(self.neg_log_prob * self.discounted_episode_rewards_)
+                self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits(
+                    logits=self.logits, labels=self.actions)
+                self.loss = tf.reduce_mean(
+                    self.neg_log_prob * self.discounted_episode_rewards_)
 
             with tf.name_scope("traing"):
-                self.train_opt = tf.trains.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+                self.train_opt = tf.trains.RMSPropOptimizer(
+                    self.learning_rate).minimize(self.loss)
+
 
 tf.reset_default_graph()
 PGNetwork = PGNetwork(state_size, action_size, learning_rate)
@@ -207,3 +214,8 @@ PGNetwork = PGNetwork(state_size, action_size, learning_rate)
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
+
+writer = tf.summary.FileWrtier("/tensorboard/pg/test")
+tf.summary.scalar("Loss", PGNetwork.loss)
+tf.summary.scalar("Reward_mean", PGNetwork.mean_rewards_)
+write_op = tf.summary.merge_all()
