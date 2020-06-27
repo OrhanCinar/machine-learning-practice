e2dcb6db430fc6a5e7b62de764ce017841bfec0c
Orhan CINAR <orhancinar35@gmail.com>
2020-06-27 03:46:31 +0200

    update model

--- SonicA2C/model.py
+++ SonicA2C/model.py
@@ -138,3 +138,105 @@ class Runner(AbtractEnvRunner):
 def sf01(arr):
     s = arr.shape
     return arr.swapaxes(0, 1).reshape(s[0]*s[1], *s[2:])
+
+
+def learn(policy,
+          env,
+          nsteps,
+          total_timesteps,
+          gamma,
+          lam,
+          vf_coef,
+          ent_coef,
+          lr,
+          max_grad_norm,
+          log_interval
+          ):
+    noptepochs = 4
+    nminibatches = 8
+    nenvs = env.num_envs
+
+    ob_space = env.observation_space
+    ac_space = env.action_space
+
+    batch_size = nenvs * nsteps
+    batch_train_size = batch_size // nminibatches
+
+    assert batch_size % nminibatches == 0
+
+    model = Model(policy=policy, ob_space=ob_space, action_space=ac_space, nenvs=nenvs,
+                  nsteps=nsteps, ent_coef=ent_coef, vf_coef=vf_coef, max_grad_norm=max_grad_norm)
+
+    load_path = "./models/260/model.ckpt"
+    model.load(load_path)
+
+    runner = Runner(env, model, nsteps, total_timesteps, gamma, lam)
+
+    tfirststart = time.time()
+
+    for update in range(1, total_timesteps//batch_size+1):
+        tstart = time.time()
+        obs, actions, returns, values = runner.run()
+        mb_losses = []
+        total_batches_train = 0
+        indices = np.arange(batch_size)
+
+        for _ in range(noptepochs):
+            np.random.shuffle(indices)
+
+            for start in range(0, batch_size, batch_train_size):
+                end = start + batch_train_size
+                mbinds = indices[start:end]
+                slices = (arr[mbinds]
+                          for arr in (obs, actions, returns, values))
+                mb_losses.append(model.train(*slices, lr))
+
+        lossvalues = np.mean(mb_losses, axis=0)
+        tnow = time.time()
+
+        fps = int(batch_size / (tnow - tstart))
+
+        if update % log_interval == 0 or update == 1:
+            ev = explained_variance(values, returns)
+            logger.record_tabular("nupdates", update)
+            logger.record_tabular("total_timesteps", update*batch_size)
+            logger.record_tabular("fps", fps)
+            logger.record_tabular("policy_loss", float(lossvalues[0]))
+            logger.record_tabular("policy_entropy", float(lossvalues[2]))
+            logger.record_tabular("value_loss", float(lossvalues[1]))
+            logger.record_tabular("explained_variance", float(ev))
+            logger.record_tabular("time elapsed", float(tnow - tfirststart))
+            logger.dump_tabular()
+
+            savepath = "./models/" + str(update) + "/model.ckpt"
+            model.save(savepath)
+            print('Saving to', savepath)
+
+    env.close()
+
+
+def play(policy, env):
+    ob_space = env.observation_space
+    ac_space = env.action_space
+
+    model = Model(policy, ob_space, ac_space, nenvs=1, nsteps=1,
+                  ent_coef=0, vf_coef=0, max_grad_norm=0)
+
+    load_path = "./model/260/model.ckpt"
+    model.load(load_path)
+
+    obs = env.reset()
+
+    score = 0
+    boom = 0
+    done = False
+
+    while done == False:
+        boom += 1
+        actions, values = model.step(obs)
+        obd, rewards, done, _ = env.step(actions)
+        score += rewards
+        env.render()
+
+    print("Score : ", score)
+    env.close()
