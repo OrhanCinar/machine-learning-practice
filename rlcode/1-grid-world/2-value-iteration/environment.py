c58fc5e4f57078d2b70653ce15ffd46af002f979
Orhan CINAR <orhancinar35@gmail.com>
2020-07-08 16:43:24 +0200

    update env

--- rlcode/1-grid-world/2-value-iteration/environment.py
+++ rlcode/1-grid-world/2-value-iteration/environment.py
@@ -205,3 +205,52 @@ class GraphicDisplay(tk.Tk):
             self.canvas.delete(i)
         self.agent.value_iteration()
         self.print_values(self.agent.value_table)
+
+    def print_optimal_policy(self):
+        self.improvement_count += 1
+        for i in self.arrows:
+            self.canvas.delete(i)
+
+        for state in self.env.get_all_states():
+            action = self.agent.get_action(state)
+            self.draw_from_values(state, action)
+
+
+class Env:
+    def __init__(self):
+        self.transition_probability = TRANSITION_PROB
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.reward = [[0] * WIDTH for _ in range(HEIGHT)]
+        self.possible_actions = POSSIBLE_ACTIONS
+        self.reward[2][2] = 1
+        self.reward[1][2] = -1
+        self.reward[2][1] = -1
+        self.all_state = []
+
+        for x in range(WIDTH):
+            for y in range(HEIGHT):
+                state = [x, y]
+                self.all_state.append(state)
+
+    def get_reward(self, state, action):
+        next_state = self.state_after_action(state, action)
+        return self.reward[next_state[0]][next_state[1]]
+
+    def state_after_action(self, state, action_index):
+        action = ACTIONS[action_index]
+        return self.check_boundary([state[0] + action[0], state[1] + action[1]])
+
+    @staticmethod
+    def check_boundary(state):
+        state[0] = (0 if state[0] < 0 else WIDTH-1
+                    if state[0] > WIDTH - 1 else state[0])
+        state[1] = (0 if state[1] < 0 else HEIGHT-1
+                    if state[1] > HEIGHT - 1 else state[1])
+        return state
+
+    def get_transaction_prob(self, state, action):
+        return self.transition_probability
+
+    def get_all_states(self):
+        return self.all_state
