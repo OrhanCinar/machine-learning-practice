6cc9e7409ca9b0aeeee86264fbe63bf091bb36ab
Orhan CINAR <orhancinar35@gmail.com>
2020-07-23 15:50:42 +0200

    add deep sarsa agent

---
+++ rlcode/1-grid-world/6-deep-sarsa/deep_sarsa_agent.py
@@ -0,0 +1,10 @@
+import copy
+import pylab
+import random
+import numpy as np
+from environment import Env
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import Sequential
+
+EPISODES = 1000
