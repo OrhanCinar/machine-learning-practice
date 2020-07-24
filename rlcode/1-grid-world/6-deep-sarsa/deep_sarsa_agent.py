309332fba3681c87a1946fcbbbd1ade640f318d9
Orhan CINAR <orhancinar35@gmail.com>
2020-07-24 18:08:27 +0200

    update agent

--- rlcode/1-grid-world/6-deep-sarsa/deep_sarsa_agent.py
+++ rlcode/1-grid-world/6-deep-sarsa/deep_sarsa_agent.py
@@ -8,3 +8,21 @@ from keras.optimizers import Adam
 from keras.models import Sequential
 
 EPISODES = 1000
+
+
+class DeepSARSAAgent:
+    def __init__(self):
+        self.load_model = false
+        self.action_space = [0, 1, 2, 3, 4]
+        self.action_size = len(self.action_space)
+        self.state_size = 15
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+        self.epsilon = 1.
+        self.epsilon_decay = .9999
+        self.epsilon_min = 0.01
+        self.model = self.build_model()
+
+        if self.load_model:
+            self.epsilon = 0.05
+            self.model.load_weights('./save_model/deep_sarsa_trained.h5')
