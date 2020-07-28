0fc32e224f719381d04797ad5f2bdcba51005fc9
Orhan CINAR <orhancinar35@gmail.com>
2020-07-28 23:24:30 +0200

    update agent

--- rlcode/1-grid-world/7-reinforce/reinforce_agent.py
+++ rlcode/1-grid-world/7-reinforce/reinforce_agent.py
@@ -8,3 +8,20 @@ from keras.models import Sequential
 from keras import backend as K
 
 EPISODES = 2500
+
+
+class Reinforcement:
+    def __init__(self):
+        self.load_model = True
+        self.action_space = [0, 1, 2, 3, 4]
+        self.action_size = len(self.action_space)
+        self.state_size = 15
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+
+        self.model = self.build_model()
+        self.optimizer = self.optimizer()
+        self.states, self.actions, self.rewadrs = [], [], []
+
+        if self.load_model:
+            self.model.load_weights('./save_model/reinforce_trained.h5')
