5b31154fc937f037df053db32bb95c85e51d4a0d
Orhan CINAR <orhancinar35@gmail.com>
2020-08-04 20:53:56 +0200

    add cartpole agent

---
+++ rlcode/2-cartpole/1-dqn/cartpole_dqn.py
@@ -0,0 +1,11 @@
+import sys
+import gym
+import pylab
+import random
+import numpy as np
+from collections import deque
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import Sequential
+
+EPISODES = 300
