94666609b8dadc0f32d307e294f5353acb1b353d
Orhan CINAR <orhancinar35@gmail.com>
2020-08-08 00:09:24 +0200

    update agent

--- rlcode/2-cartpole/1-dqn/cartpole_dqn.py
+++ rlcode/2-cartpole/1-dqn/cartpole_dqn.py
@@ -17,3 +17,19 @@ class DQNAgent:
         self.load_model = False
         self.state_size = state_size
         self.action_size = action_size
+
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.999
+        self.epsilon_min = 0.01
+        self.batch_size = 64
+        self.train_start = 1000
+        self.memory = deque(maxlen=(2000))
+        self.model = self.build_model()
+        self.target_model = self.build_model()
+        self.update_tarhet_model()
+
+        if self.load_model:
+            self.model.load_weigths("./save_model/cartpole_dqn.h5")
+
