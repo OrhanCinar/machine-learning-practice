2ed069da4786000e877464f17b59fa7539711a02
Orhan CINAR <orhancinar35@gmail.com>
2020-08-18 22:59:42 +0200

    update a2c cartpole

--- rlcode/2-cartpole/4-actor-critic/cartpole_a2c.py
+++ rlcode/2-cartpole/4-actor-critic/cartpole_a2c.py
@@ -9,3 +9,23 @@ from keras.optimizers import Adam
 EPISODES = 1000
 
 # https://github.com/rlcode/reinforcement-learning/blob/master/2-cartpole/4-actor-critic/cartpole_a2c.py
+
+
+class A2CAgent:
+    def __init__(self, state_size, action_size):
+        self.render = False
+        self.load_model = False
+        self.state_size = state_size
+        self.action_size = action_size
+        self.value_size = 1
+
+        self.discount_factor = 0.99
+        self.actor_lr = 0.001
+        self.critic_lr = 0.005
+
+        self.action = self.build_actor()
+        self.critic = self.build_critic()
+
+        if self.load_model:
+            self.actor.load_weights("./save_model/cartpole_actor.h5")
+            self.critic.load_weights("./save_model/cartpole_critic.h5")
