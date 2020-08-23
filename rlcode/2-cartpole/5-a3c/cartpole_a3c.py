900d0b33d1f772e358c59911391078efab1d1795
Orhan CINAR <orhancinar35@gmail.com>
2020-08-23 18:59:32 +0200

    upadate agent

--- rlcode/2-cartpole/5-a3c/cartpole_a3c.py
+++ rlcode/2-cartpole/5-a3c/cartpole_a3c.py
@@ -15,3 +15,23 @@ episode = 0
 scores = []
 
 EPISODES = 2000
+
+
+class A3CAgent:
+    def __init__(self, state_size, action_size, env_name):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.env_name = env_name
+
+        self.actor_lr = 0.001
+        self.critic_lr = 0.001
+        self.discount_factor = 0.90
+        self.hidden1, self.hidden2 = 24, 24
+        self.threads = 0
+
+        self.actor, self.critic = self.build_model()
+
+        self.optimizer = [self.actor_optimizer(), self.critic_optimizer()]
+        self.sess = tf.InteractiveSession()
+        K.set_session(self.sess)
+        self.sess.run(tf.global_variables_initializer())
