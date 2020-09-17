6982f16be40f0b27952ce541ca4f7230de476461
Orhan CINAR <orhancinar35@gmail.com>
2020-09-17 14:21:35 +0200

    add ddqn breakout agent

---
+++ rlcode/3-atari/1-breakout/breakout_ddqn.py
@@ -0,0 +1,53 @@
+import gym
+import random
+import numpy as np
+import tensorflow as tf
+from collections import deque
+from skimage.color import rgb2gray
+from skimage.transform import resize
+from keras.models import Sequential
+from keras.optimizers import RMSprop
+from keras.layers import Dense, Flatten
+from keras.layers.convolutional import Conv2D
+from keras import backend as K
+
+EPISODES = 50000
+
+# https://github.com/rlcode/reinforcement-learning/blob/master/3-atari/1-breakout/breakout_ddqn.py
+
+
+class DDQAgent:
+    def __init__(self, action_size):
+        self.render = False
+        self.load_model = False
+
+        self.state_size = (84, 84, 4)
+        self.action_size = action_size
+
+        self.epsilon = 1.
+        self.epsilon_start, self.epsilon_end = 1.0, 0.1
+        self.exploration_steps = 1000000.
+        self.epsilon_decay_step = (self.epsilon_start - self.epsilon_end) / self.exploration_steps
+
+        self.batch_size = 32
+        self.train_start = 50000
+        self.update_target_rate = 10000
+        self.discount_factor = 0.99
+        self.memory = deque(maxlen=400000)
+        self.no_op_states = 30
+
+        self.model = self.build_model()
+        self.update_target_model()
+
+        self.optimizer = self.optimizer()
+
+        self.sess = tf.InteractionSession()
+        K.set_session(self.sess)
+
+        self.avg_q_max, self.avg_loss = 0, 0
+        self.summary_placeholders, self.update_ops, self.summary_op = self.setup_summary()
+        self.summary_writer = tf.summary.FileWriter('summary/breakout_ddqn', self.sess_grap)
+        self.sess.run(tf.global_variables_initializer())
+
+        if self.load_model:
+            self.model.load_weights("./save_model/breakout_ddqn.h5")
