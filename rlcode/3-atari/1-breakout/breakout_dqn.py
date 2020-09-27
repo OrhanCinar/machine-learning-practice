1961e36b6e0e4ee25d4b1665cd88b23ff07e0e71
Orhan CINAR <orhancinar35@gmail.com>
2020-09-27 17:40:57 +0200

    update agent

--- rlcode/3-atari/1-breakout/breakout_dqn.py
+++ rlcode/3-atari/1-breakout/breakout_dqn.py
@@ -68,7 +68,16 @@ class DQNAgent:
         return train
 
     def build_model(self):
-        pass
+        model = Sequential()
+        model.add(Conv2D(32, (8, 8), strides=(4, 4),
+                         activation='relu', input_shape=self.state_size))
+        model.add(Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))
+        model.add(Conv2D(64, (3, 3), strides=(1, 1), activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(self.action_size))
+        model.summary()
+        return model
 
     def update_target_model(self):
         pass
