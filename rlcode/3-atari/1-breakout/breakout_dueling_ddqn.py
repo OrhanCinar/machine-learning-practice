b7347de3103bbb29ce0a418feee07a8cf7c0e1b9
Orhan CINAR <orhancinar35@gmail.com>
2020-10-03 16:39:17 +0200

    update agent

--- rlcode/3-atari/1-breakout/breakout_dueling_ddqn.py
+++ rlcode/3-atari/1-breakout/breakout_dueling_ddqn.py
@@ -27,3 +27,31 @@ class DuelingDDQNAgent:
         self.exploration_steps = 1000000.
         self.epsilon_decay_step = (
             self.epsilon_start - self.epsilon_end)/self.exploration_steps
+
+        self.batch_size = 32
+        self.train_start = 50000
+        self.update_target_rate = 10000
+        self.discount_factor = 0.99
+        self.memory = deque(maxlen=400000)
+        self.no_op_steps = 30
+        self.model = self.build_model()
+        self.target_model = self.build_model()
+        self.optimizer = self.optimizer()
+        self.sess = tf.InteractiveSession()
+        K.set_session(self.sess)
+
+        self.avg_q_max, self.avg_loss = 0, 0
+        self.summary_placeholders, self.update_ops,
+        self.summary_op = self.setup_summary()
+        self.summary_writer = tf.summary.FileWriter(
+            'summary/breakout_dueling_ddqn', self.sess.graph)
+        self.sess.run(tf.global_variables.initializer())
+
+        if self.load_model:
+            self.model.load_weights("./save_model/breakout_dueling_ddqb.h5")
+
+    def optimizer(self):
+        pass
+
+    def build_model(self):
+        pass
