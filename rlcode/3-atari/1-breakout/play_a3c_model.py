313c7c6c8f99931604cbdf38a836e8cfe9d4f845
Orhan CINAR <orhancinar35@gmail.com>
2020-10-12 15:04:00 +0200

    update agent

--- rlcode/3-atari/1-breakout/play_a3c_model.py
+++ rlcode/3-atari/1-breakout/play_a3c_model.py
@@ -11,3 +11,19 @@ global episode
 episode = 0
 EPISODES = 8000000
 env_name = "BreakoutDeterministic-v4"
+
+# https://github.com/rlcode/reinforcement-learning/blob/master/3-atari/1-breakout/play_a3c_model.py
+
+
+class TestAgent:
+    def __init__(self, action_size):
+        self.state_size = (84, 84, 4)
+        self.action_size = action_size
+
+        self.discount_factor = 0.99
+        self.no_op_steps = 30
+
+        self.acttor, self.critic = self.build_model()
+
+    def build_model(self):
+        pass
