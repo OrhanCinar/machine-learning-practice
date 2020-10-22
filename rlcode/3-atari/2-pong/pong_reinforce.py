6507eda85db62701d0215e56619ce5d3f87fa780
Orhan CINAR <orhancinar35@gmail.com>
2020-10-22 15:13:17 +0200

    add pong agent

---
+++ rlcode/3-atari/2-pong/pong_reinforce.py
@@ -0,0 +1,34 @@
+import gym
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Reshape, Flatten
+from keras.optimizers import Adam
+from keras.layers.convolutional import Convolution2D
+
+
+class PGAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.gamma = 0.99
+        self.learning_rate = 0.001
+        self.states = []
+        self.gradients = []
+        self.rewards = []
+        self.probs = []
+        self.model = self._build_model()
+        self.model.summary()
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Reshape((1, 80, 60), input_shape=(self.state_size,)))
+        model.add(Connvolution2D(32, 6, 6, subsample=(3, 3),
+                                 border_mode='same', activation='relu', init='he_uniform'))
+
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu', init='he_uniform'))
+        model.add(Dense(32, activation='relu', init='he_uniform'))
+        model.add(Dense(self.action_size, activation='softmax'))
+        opt = Add(lr=self.learning_rate)
+        model.compile(loss='categorical_crossentropy', optimizer=opt)
+        return model
