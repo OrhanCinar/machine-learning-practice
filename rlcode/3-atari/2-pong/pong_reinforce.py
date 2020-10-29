214e84f9d6a2d4ee5d24d31f3d6f667a1f1e3efa
Orhan CINAR <orhancinar35@gmail.com>
2020-10-29 16:35:56 +0100

    update agent

--- rlcode/3-atari/2-pong/pong_reinforce.py
+++ rlcode/3-atari/2-pong/pong_reinforce.py
@@ -87,3 +87,39 @@ def preprocess(I):
     I[I == 109] = 0
     I[I != 0] = 1
     return I.astype(np.float).ravel()
+
+
+if __name__ == "__main__":
+    env = gym.make("Pong-v0")
+    state = env.reset()
+    prev_x = None
+    score = 0
+    episode = 0
+
+    state_size = 80 * 80
+    action_size = env.action_space.n
+    agent = PGAgent(state_size, action_size)
+    agent.load('./save_model/pong_reinforce.h5')
+
+    while True:
+        env.render()
+
+        cur_x = preprocess(state)
+        x = cur_x - prev_x if prev_x is not None else np.zeros(state_size)
+        prev_x = cur_x
+
+        action, prob = agent.act(x)
+        state, reward, done, info = env.step(action)
+        score += reward
+        agent.remember(x, action, prob, reward)
+
+        if done:
+            episode += 1
+            agent.train()
+            print('Episode: %d - Score: %f.' % (episode, score))
+            score = 0
+            state = env.reset()
+            prev_x = None
+
+            if episode > 1 and episode % 10 == 0:
+                agent.save('./save_model/pong_reinforce.h5')
