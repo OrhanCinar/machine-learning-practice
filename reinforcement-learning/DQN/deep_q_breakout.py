c949be5ba4f32440ac60a70101963f58d2845749
Orhan CINAR <orhancinar35@gmail.com>
2020-11-12 03:39:51 +0100

    update agent

--- reinforcement-learning/DQN/deep_q_breakout.py
+++ reinforcement-learning/DQN/deep_q_breakout.py
@@ -34,3 +34,73 @@ class StateProcessor():
 
     def process(self, sess, state):
         return sess.run(self.output, {self.input_state: state})
+
+
+class Estimator():
+    def __init__(self, scope="estimator", summaries_dir=None):
+        self.scope = scope
+        self.summary_writer = None
+        with tf.variable_scope(scope):
+            self._build_model()
+            if summaries_dir:
+                summary_dir = os.path.join(
+                    summaries_dir, "summaries_()".format(scope))
+                if not os.path.exists(summary_dir):
+                    os.makedirs(summary_dir)
+                self.summary_writer = tf.summary.FileWriter(summaries_dir)
+
+    def _build_model(self):
+        self.X_pl = tf.placeholder(
+            shape=[None, 84, 84, 4], dtype=tf.uint8, name="X")
+        self.y_pl = tf.placeholder(shape=[None], dtype=tf.float32, name="y")
+        self.action_pl = tf.placeholder(
+            shape=[None], dtype=tf.int32, name="actions")
+
+        X = tf.to_float(self.X_pl) / 255.0
+        batch_size = tf.shape(self.X_pl)[0]
+
+        conv1 = tf.contrib.layers.conv2d(X, 32, 8, 4, activation_fn=tf.nn.relu)
+        conv2 = tf.contrib.layers.conv2d(
+            conv1, 64, 4, 2, activation_fn=tf.nn.relu)
+        conv3 = tf.contrib.layers.conv2d(
+            conv2, 64, 3, 1, activation_fn=tf.nn.relu)
+
+        flattened = tf.contrib.layers.flatten(conv3)
+        fc1 = tf.contrib.layers.fully_connected(flattened, 512)
+        self.predictions = tf.contrib.layers.fully_connected(
+            fc1, len(VALID_ACTIONS))
+
+        gather_indices = tf.range(
+            batch_size) * tf.shape(self.predictions)[1] + self.actions_pl
+        self.action_predictions = tf.gather(
+            tf.reshape(self.predictions, [-1], gathet_indices))
+
+        self.losses = tf.squared_difference(self.y_pl, self.action_predictions)
+        self.loss = tf.reduce_mean(self.losses)
+
+        self.optimizer = tf.squared_difference(
+            self.y_pl, self.action_predictions)
+        self.loss = tf.reduce_mean(self.losses)
+
+        self.optimizer = tf.train.RMSPropOptimizer(0.00025, 0.99, 0.0, 1e-6)
+        self.train_op = self.optimizer.minimize(
+            self.loss, global_step=tf.contrib.framework.get_global_step())
+
+        self.summaries = tf.summary.merge([
+            tf.summary.scalar("loss", self.loss),
+            tf.summary.histogram("loss_hist", self.losses),
+            tf.summary.histogram("q_values_hist", self.predictions),
+            tf.summary.scalar("max_q_value", tf.reduce_max(self.predictions))
+        ])
+
+    def predict(self, sess, s):
+        return sess.run(self.predictions, {self.X_pl: s})
+
+    def update(self, sess, s, a, y):
+        feed_dict = {self.X_pl: s, self.y_pl: y, self.actions_pl: a}
+        summaries, global_step, _, loss = sess.run(
+            [self.summaries, tf.contrib.framework.get_global_step(),
+             self.train_op, self.loss], feed_dict)
+        if self.summary_write:
+            self.summary_writer.add_summary(summaries, global_step)
+        return loss
