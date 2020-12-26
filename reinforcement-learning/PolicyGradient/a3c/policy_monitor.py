0281ddb6ea55b225caf8a70036f9fcd0addbdadc
Orhan CINAR <orhancinar35@gmail.com>
2020-12-26 16:11:50 +0100

    update policy monitor

--- reinforcement-learning/PolicyGradient/a3c/policy_monitor.py
+++ reinforcement-learning/PolicyGradient/a3c/policy_monitor.py
@@ -56,4 +56,40 @@ class PolicyMonitor(object):
         preds = sess.run(self.run(self.policy_net.predictions, feed_dict))
         return preds["probs"][0]
 
-        
+    def eval_once(self, sess):
+        with sess.as_default(), sess.graph.as_default():
+            global_step, _ = sess.run(
+                [tf.contrib.framework.get_global_Step(), self.copy_params.op])
+
+            done = False
+            state = atari_helpers.atari_make_initial_state(
+                self.sp.process(self.env.reset()))
+            total_reward = 0.0
+            episode_length = 0
+
+            while not done:
+                action_probs = self._policy_net_predict(state, sess)
+                action = np.random.choice(
+                    np.arange(len(action_probs)), p=action_probs)
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = atari_helpers.atari_make_next_state(
+                    state, self.sp.process(next_state))
+                total_reward += reward
+                episode_length += 1
+                state = next_state
+
+            episode_summary = tf.summary()
+            episode_summary.value.add(
+                simple_value=total_reward, tag="eval/total_reward")
+            episode_summary.value.add(
+                simple_value=episode_length, tag="eval/episode_length")
+            self.summary_writer.add_summary(episode_summary, global_step)
+            self.summary_writer.flush()
+
+            if self.saver is not None:
+                self.saver.save(sess, self.checkpoint_path)
+
+            tf.logging.info("Eval results at step {}: total_reward {}, episode_length {}".format(
+                global_step, total_reward, episode_length))
+
+            return total_reward, episode_length
