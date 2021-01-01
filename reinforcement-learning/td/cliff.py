efdbf4e983950d18219047fb47c58b73ec604e8d
Orhan CINAR <orhancinar35@gmail.com>
2021-01-01 17:52:35 +0100

    add td cliff

---
+++ reinforcement-learning/td/cliff.py
@@ -0,0 +1,7 @@
+import gym
+import numpy as np
+import sys
+
+from lib.envs.cliff_walking import CliffWalkingEnv
+
+# https://github.com/dennybritz/reinforcement-learning/blob/master/TD/Cliff%20Environment%20Playground.ipynb
