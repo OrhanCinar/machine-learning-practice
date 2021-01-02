40e55b29ce558f12874abee2fddb4589384d7751
Orhan CINAR <orhancinar35@gmail.com>
2021-01-02 22:37:04 +0100

    update q learning cliff

--- reinforcement-learning/td/q_cliff.py
+++ reinforcement-learning/td/q_cliff.py
@@ -1,7 +1,22 @@
-import gym
-import numpy as np
-import sys
-
+from lib import plotting
 from lib.envs.cliff_walking import CliffWalkingEnv
+from collections import defaultdict
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib
+import itertools
+import gym
+%matplotlib inline
+
+
+if "../" not in sys.path:
+    sys.path.append("../")
+
+
+matplotlib.style.use('ggplot')
 
 # https://github.com/dennybritz/reinforcement-learning/blob/master/TD/Cliff%20Environment%20Playground.ipynb
+
+
+env = CliffWalkingEnv()
